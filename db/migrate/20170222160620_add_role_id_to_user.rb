6484ea67b97bf7dc22fe5184beab9c5a5124b625
Moodzii <mahmoudhakh@gmail.com>
2017-02-23 15:59:10 +0000

    Try 3 of wroking roles and parent table

--- db/migrate/20170222160620_add_role_id_to_user.rb
+++ db/migrate/20170222160620_add_role_id_to_user.rb
@@ -1,5 +1,6 @@
 class AddRoleIdToUser < ActiveRecord::Migration
   def change
+    add_column  :users, :roles, :string, :default => 0
     add_reference :users, :role, index: true, foreign_key: true
   end
 end
